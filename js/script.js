cbb203981a6d34aa54b77ed7386db51892ce953f
Brayden King <braywarjust123@gmail.com>
2025-03-26 21:14:46 -0300

    V2.1.1

---
+++ js/script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const navToggle = document.querySelector('.nav-toggle');
+    const navMenu = document.querySelector('.nav-menu');
+
+    navToggle.addEventListener('click', function () {
+        navMenu.classList.toggle('active'); // Toggles the 'active' class to show/hide the menu
+    });
+});
